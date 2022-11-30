fa9c01c131112ac9028af09f05466b0a71a141c3
cjrequena <cjrequena@protonmail.com>
2022-11-30 16:38:33 +0100

    --

--- spring-boot-data-mongodb-sample/src/main/java/com/cjrequena/sample/db/entity/FooEntity.java
+++ spring-boot-data-mongodb-sample/src/main/java/com/cjrequena/sample/db/entity/FooEntity.java
@@ -2,7 +2,10 @@ package com.cjrequena.sample.db.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
@@ -19,14 +22,17 @@ import java.time.LocalDate;
 @Getter
 public class FooEntity {
 
-
+  @Id
+  @Field(name = "id")
   private String id;
 
-  @NotNull
+  @Field(name = "name")
   private String name;
 
+  @Field(name = "description")
   private String description;
 
+  @Field(name = "creation_date")
   private LocalDate creationDate;
 
 }
