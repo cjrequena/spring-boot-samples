e06d5d229a2662e7d944c5e8f2354354344da72f
cjrequena <cjrequena@protonmail.com>
2023-06-12 17:31:27 +0200

    --

--- spring-boot-data-jpa-h2-sample/src/main/java/com/cjrequena/sample/web/api/FooApi.java
+++ spring-boot-data-jpa-h2-sample/src/main/java/com/cjrequena/sample/web/api/FooApi.java
@@ -46,7 +46,17 @@ public class FooApi {
     summary = "Create a new foo.",
     description = "Create a new foo.",
     parameters = {
-      @Parameter(name = "Accept-Version", required = true, in = ParameterIn.HEADER, schema = @Schema(name = "Accept-Version", type = "string", implementation = String.class, allowableValues = {VND_SAMPLE_SERVICE_V1}))
+      @Parameter(
+        name = "Accept-Version",
+        required = true,
+        in = ParameterIn.HEADER,
+        schema = @Schema(
+          name = "Accept-Version",
+          type = "string",
+          implementation = String.class,
+          allowableValues = {VND_SAMPLE_SERVICE_V1}
+        )
+      )
     },
     requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(required = true, content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = FooDTO.class)))
   )
@@ -78,6 +88,34 @@ public class FooApi {
 
   }
 
+  @Operation(
+    summary = "Get a foo by id.",
+    description = "Get a foo by id.",
+    parameters = {
+      @Parameter(
+        name = "Accept-Version",
+        required = true, in =
+        ParameterIn.HEADER,
+        schema = @Schema(
+          name = "Accept-Version",
+          type = "string",
+          allowableValues = {VND_SAMPLE_SERVICE_V1}
+        )
+      )
+    }
+  )
+  @ApiResponses(
+    value = {
+      @ApiResponse(responseCode = "200", description = "OK - The request was successful and the response body contains the representation requested."),
+      @ApiResponse(responseCode = "400", description = "Bad Request - The data given in the GET failed validation. Inspect the response body for details."),
+      @ApiResponse(responseCode = "401", description = "Unauthorized - The supplied credentials, if any, are not sufficient to access the resource."),
+      @ApiResponse(responseCode = "404", description = "Not Found"),
+      @ApiResponse(responseCode = "408", description = "Request Timeout"),
+      @ApiResponse(responseCode = "429", description = "Too Many Requests - Your application is sending too many simultaneous requests."),
+      @ApiResponse(responseCode = "500", description = "Internal Server Error - We couldn't return the representation due to an internal server error."),
+      @ApiResponse(responseCode = "503", description = "Service Unavailable - We are temporarily unable to return the representation. Please wait for a bit and try again."),
+    }
+  )
   @GetMapping(
     path = "/fooes/{id}",
     produces = {APPLICATION_JSON_VALUE}
@@ -94,6 +132,34 @@ public class FooApi {
     }
   }
 
+  @Operation(
+    summary = "Get a list of fooes.",
+    description = "Get a list of fooes.",
+    parameters = {
+      @Parameter(
+        name = "Accept-Version",
+        required = true, in =
+        ParameterIn.HEADER,
+        schema = @Schema(
+          name = "Accept-Version",
+          type = "string",
+          allowableValues = {VND_SAMPLE_SERVICE_V1}
+        )
+      )
+    }
+  )
+  @ApiResponses(
+    value = {
+      @ApiResponse(responseCode = "200", description = "OK - The request was successful and the response body contains the representation requested."),
+      @ApiResponse(responseCode = "400", description = "Bad Request - The data given in the GET failed validation. Inspect the response body for details."),
+      @ApiResponse(responseCode = "401", description = "Unauthorized - The supplied credentials, if any, are not sufficient to access the resource."),
+      @ApiResponse(responseCode = "404", description = "Not Found"),
+      @ApiResponse(responseCode = "408", description = "Request Timeout"),
+      @ApiResponse(responseCode = "429", description = "Too Many Requests - Your application is sending too many simultaneous requests."),
+      @ApiResponse(responseCode = "500", description = "Internal Server Error - We couldn't return the representation due to an internal server error."),
+      @ApiResponse(responseCode = "503", description = "Service Unavailable - We are temporarily unable to return the representation. Please wait for a bit and try again."),
+    }
+  )
   @GetMapping(
     path = "/fooes",
     produces = {APPLICATION_JSON_VALUE}
@@ -105,6 +171,36 @@ public class FooApi {
     return new ResponseEntity<>(dtoList, responseHeaders, HttpStatus.OK);
   }
 
+  @Operation(
+    summary = "Update a foo by id.",
+    description = "Update a foo by id.",
+    parameters = {
+      @Parameter(
+        name = "Accept-Version",
+        required = true, in =
+        ParameterIn.HEADER,
+        schema = @Schema(
+          name = "Accept-Version",
+          type = "string",
+          allowableValues = {VND_SAMPLE_SERVICE_V1}
+        )
+      )
+    },
+    requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(required = true, content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = FooDTO.class)))
+  )
+  @ApiResponses(
+    value = {
+      @ApiResponse(responseCode = "200", description = "OK - The request was successful, we updated the resource and the response body contains the representation."),
+      @ApiResponse(responseCode = "204", description = "No Content - The request was successful, we created a new resource and the response body does not contains the representation."),
+      @ApiResponse(responseCode = "400", description = "Bad Request - The data given in the PUT failed validation. Inspect the response body for details."),
+      @ApiResponse(responseCode = "401", description = "Unauthorized - The supplied credentials, if any, are not sufficient to access the resource."),
+      @ApiResponse(responseCode = "408", description = "Request Timeout"),
+      @ApiResponse(responseCode = "409", description = "Conflict - The request could not be processed because of conflict in the request"),
+      @ApiResponse(responseCode = "429", description = "Too Many Requests - Your application is sending too many simultaneous requests."),
+      @ApiResponse(responseCode = "500", description = "Internal Server Error - We couldn't create the resource. Please try again."),
+      @ApiResponse(responseCode = "503", description = "Service Unavailable - We are temporarily unable. Please wait for a bit and try again. ")
+    }
+  )
   @PutMapping(
     path = "/fooes/{id}",
     produces = {APPLICATION_JSON_VALUE}
@@ -122,6 +218,33 @@ public class FooApi {
     }
   }
 
+  @Operation(
+    summary = "Delete a foo by id.",
+    description = "Delete a foo by id.",
+    parameters = {
+      @Parameter(
+        name = "Accept-Version",
+        required = true, in =
+        ParameterIn.HEADER,
+        schema = @Schema(
+          name = "Accept-Version",
+          type = "string",
+          allowableValues = {VND_SAMPLE_SERVICE_V1}
+        )
+      )
+    }
+  )
+  @ApiResponses(
+    value = {
+      @ApiResponse(responseCode = "204", description = "OK - The request was successful; the resource was deleted."),
+      @ApiResponse(responseCode = "401", description = "Unauthorized - The supplied credentials, if any, are not sufficient to access the resource."),
+      @ApiResponse(responseCode = "404", description = "Not Found"),
+      @ApiResponse(responseCode = "408", description = "Request Timeout"),
+      @ApiResponse(responseCode = "429", description = "Too Many Requests - Your application is sending too many simultaneous requests."),
+      @ApiResponse(responseCode = "500", description = "Internal Server Error - We couldn't delete the resource. Please try again."),
+      @ApiResponse(responseCode = "503", description = "Service Unavailable")
+    }
+  )
   @DeleteMapping(
     path = "/fooes/{id}",
     produces = {APPLICATION_JSON_VALUE}
