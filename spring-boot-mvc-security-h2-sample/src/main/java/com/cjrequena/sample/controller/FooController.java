a6ef25913dfa573e02b4120d94d6626b22b4fac1
cjrequena <carlosjose.requena@tui.com>
2024-03-27 14:35:34 +0100

    --

---
+++ spring-boot-mvc-security-h2-sample/src/main/java/com/cjrequena/sample/controller/FooController.java
@@ -0,0 +1,28 @@
+package com.cjrequena.sample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FooController {
+
+  @GetMapping("/home")
+  public String handleWelcome() {
+    return "home";
+  }
+
+  @GetMapping("/admin/home")
+  public String handleAdminHome() {
+    return "admin_home";
+  }
+
+  @GetMapping("/user/home")
+  public String handleUserHome() {
+    return "user_home";
+  }
+
+  @GetMapping("/login")
+  public String handleLogin() {
+    return "custom_login";
+  }
+}
