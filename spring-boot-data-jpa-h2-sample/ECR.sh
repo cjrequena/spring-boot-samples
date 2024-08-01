73f28d865d6a9465180f75de347e8c41e1434715
cjrequena <carlosjose.requena@tui.com>
2024-08-01 14:34:26 +0200

    --

---
+++ spring-boot-data-jpa-h2-sample/ECR.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set environment variables
+export ECR_URL=296754545385.dkr.ecr.us-east-1.amazonaws.com
+export ECR_REPOSITORY=cjrequena/spring-boot-data-jpa-h2-sample
+export DOCKER_IMAGE_VERSION=001
+export IMAGE=${ECR_URL}/${ECR_REPOSITORY}:${DOCKER_IMAGE_VERSION}
+
+# Authenticate Docker to the Amazon ECR registry
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ECR_URL}
+
+# Build the Docker image
+docker build -t ${IMAGE} .
+
+# Push the Docker image to ECR
+docker push ${IMAGE}
