7dc2fe10a466db1f03de8239496588ef43897730
cjrequena <carlosjose.requena@tui.com>
2024-08-20 09:31:21 +0200

    --

--- spring-boot-data-jpa-h2-sample/ecr-mac-m1.sh
+++ spring-boot-data-jpa-h2-sample/ecr-mac-m1.sh
@@ -9,10 +9,15 @@ export IMAGE=${ECR_URL}/${ECR_REPOSITORY}:${DOCKER_IMAGE_VERSION}
 # Authenticate Docker to the Amazon ECR registry
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ECR_URL}
 
-# Build the Docker image
+# Create docker builder
 docker buildx create --name aws-linux-builder
+
+# Build the Docker image
 docker buildx use aws-linux-builder
 docker buildx build --platform linux/amd64,linux/arm64 -t ${IMAGE} .
 
 # Push the Docker image to ECR
 docker buildx build --push --platform linux/amd64,linux/arm64 -t ${IMAGE} .
+
+# Delete docker builder
+docker buildx create --name aws-linux-builder
