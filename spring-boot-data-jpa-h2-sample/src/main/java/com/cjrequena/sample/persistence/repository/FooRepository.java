48402f92760cd08842a32e5e67eaec82e1a69ff4
cjrequena <carlosjose.requena@tui.com>
2026-01-26 12:37:04 +0100

    --

--- spring-boot-data-jpa-h2-sample/src/main/java/com/cjrequena/sample/persistence/repository/FooRepository.java
+++ spring-boot-data-jpa-h2-sample/src/main/java/com/cjrequena/sample/persistence/repository/FooRepository.java
@@ -2,9 +2,12 @@ package com.cjrequena.sample.persistence.repository;
 
 import com.cjrequena.sample.persistence.entity.FooEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * <p>
@@ -20,5 +23,11 @@ public interface FooRepository extends JpaRepository<FooEntity, Long> {
   List<FooEntity> findByNameNamedNativeQueryExample(String name);
 
   List<FooEntity> findByNameNamedQueryExample(String name);
+
+  @Query(value =
+    """ 
+    SELECT * FROM Foo WHERE name = :name
+    """, nativeQuery = true)
+  List<FooEntity> findByNameNativeQueryExample(@Param("name") String name);
 
 }
